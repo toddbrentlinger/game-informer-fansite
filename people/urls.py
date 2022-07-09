85a6f178a33fa178841faf7ed95fe2868b854013
toddbrentlinger <toddbrentlinger@gmail.com>
2022-07-09 16:23:40 -0700

    Improve Person detail page
    Creates custom detail view, replacing the generic detail view, that
    passes all ReplayEpisodes including the Person.
    Adds some new fields from Person model into template HTML.
    Adds stylesheet for Person detail page.

--- people/urls.py
+++ people/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PersonListView.as_view(), name='people'),
-    path('<int:pk>/', views.PersonDetailView.as_view(), name='person-detail'),
+    #path('<int:pk>/', views.PersonDetailView.as_view(), name='person-detail'),
+    path('<int:pk>/', views.person_detail_view, name='person-detail'),
     re_path(r'(?P<stub>[-\w]+)/$', views.person_detail_slug_view, name='person-detail-slug'),
 ]
--- people/views.py
+++ people/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.db.models import Q
 from .models import Person
+from fansite.models import ReplayEpisode
 
 # Create your views here.
 
@@ -11,6 +13,19 @@ class PersonListView(generic.ListView):
 class PersonDetailView(generic.DetailView):
     model = Person
 
+def get_person_detail_view(request, person):
+    replay_episodes = ReplayEpisode.objects.filter(Q(host=person) | Q(featuring=person)).distinct()
+
+    context = {
+        'person': person,
+        'replay_episodes': replay_episodes
+    }
+    return render(request, 'people/person_detail.html', context=context)
+
+def person_detail_view(request, pk):
+    person = get_object_or_404(Person, pk=pk)
+    return get_person_detail_view(request, person)
+
 def person_detail_slug_view(request, stub):
     person = get_object_or_404(Person, slug=stub)
-    return render(request, 'people/person_detail.html', context={'person': person})
+    return get_person_detail_view(request, person)
