d16b3585aeb2fc09c12a21199bcf2184ec9cb3de
toddbrentlinger <toddbrentlinger@gmail.com>
2022-07-16 12:48:49 -0700

    Replace basic game tag to display list instead
    Instead of creating a list of custom tags every time, creates a tag
    that accepts a list of games, rather than a single game, and creates
    the list inside the tag.
    Removes old tag and template file.
    Adds to templates that display list of games.

--- games/templatetags/game_tags.py
+++ games/templatetags/game_tags.py
@@ -2,6 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('game_basic_tag.html')
-def game_basic_display(game):
-    return {'game': game}
+@register.inclusion_tag('game_basic_list_tag.html')
+def game_basic_list_display(game_list):
+    return {'game_list': game_list}
