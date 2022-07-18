a3d300bde325980ffb43251d6ccf1e97d4694ec4
toddbrentlinger <toddbrentlinger@gmail.com>
2022-07-18 12:16:37 -0700

    Improve/add game and replay episode basic list tags
    Creates inclusion tag to display basic ReplayEpisode list.
    Improves inclusion tag to display basic Game list.
    Adds model list tags to appropriate detail and list page templates.

--- replay/templatetags/replayepisode_tags.py
+++ replay/templatetags/replayepisode_tags.py
@@ -1,4 +1,3 @@
-from operator import itemgetter
 from django import template
 
 register = template.Library()
@@ -6,6 +5,10 @@ register = template.Library()
 @register.inclusion_tag('replayepisode_basic_tag.html')
 def replayepisode_basic_display(replayepisode):
     return {'replayepisode': replayepisode}
+
+@register.inclusion_tag('replayepisode_basic_list_tag.html')
+def replayepisode_basic_list_display(replayepisode_list):
+    return {'replayepisode_list': replayepisode_list}
 
 @register.inclusion_tag('heading_tag.html')
 def display_heading_json(key, value):
@@ -16,5 +19,4 @@ def display_heading_json(key, value):
             else:
                 return item
         value = map(handle_quotes_map, value)
-        value = map(lambda item: item, value)
     return {'key': key, 'value': value}
