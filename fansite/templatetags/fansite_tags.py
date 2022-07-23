7197621632b69025bda327f512fb606631929681
toddbrentlinger <toddbrentlinger@gmail.com>
2022-07-23 13:12:48 -0700

    Start page selection tag for Page objects
    Starts inclusion tag function that accepts a Page object and number of
    page buttons to display.
    Adds html template and stylesheet for page selection.
    Adds page selection to ReplayEpisode list returned from search query.

--- fansite/templatetags/fansite_tags.py
+++ fansite/templatetags/fansite_tags.py
@@ -1,3 +1,5 @@
+import math
+
 from django import template
 
 register = template.Library()
@@ -19,10 +21,38 @@ def create_video_gallery_slider(gamevideo_list):
     }
 
 @register.inclusion_tag('page_selection_tag.html')
-def create_page_selection(paginator, curr_page):
+def create_page_selection(page_obj, num_page_btns = 5):
+    curr_page = page_obj.number
+    last_page = page_obj.paginator.num_pages
+    middle_page = math.ceil(num_page_btns / 2)
+
+    prev = curr_page == 1
+    next = curr_page == last_page
+    first = last_page <= num_page_btns or curr_page <= middle_page
+    last = last_page <= num_page_btns or curr_page >= last_page - middle_page + 1
+
+    if last_page > num_page_btns:
+        if curr_page > last_page - middle_page:
+            start = last_page - num_page_btns + 1
+            end = last_page
+        elif curr_page > middle_page:
+            start = curr_page - middle_page + 1
+            end = curr_page + middle_page - 1
+        else:
+            start = 1
+            end = num_page_btns
+    else:
+        start = 1
+        end = last_page
+
     return {
-        'paginator': paginator,
-        'curr_page': curr_page,
+        'page_obj': page_obj,
+        'num_page_btns': num_page_btns,
+        'prev': prev,
+        'next': next,
+        'first': first,
+        'last': last,
+        'range': range(start, end + 1),
     }
 
 @register.filter
