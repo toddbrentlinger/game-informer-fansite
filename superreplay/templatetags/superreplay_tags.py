03329b6ed9d655f0919faf7d050234d994677874
toddbrentlinger <toddbrentlinger@gmail.com>
2022-08-14 12:57:32 -0700

    Improve templates and tags for superreplay app

--- superreplay/templatetags/superreplay_tags.py
+++ superreplay/templatetags/superreplay_tags.py
@@ -9,3 +9,11 @@ def superreplay_basic_display(superreplay):
 @register.inclusion_tag('superreplay_basic_list_tag.html')
 def superreplay_basic_list_display(superreplay_list):
     return {'superreplay_list': superreplay_list}
+
+@register.inclusion_tag('superreplayepisode_basic_tag.html')
+def superreplayepisode_basic_display(superreplayepisode):
+    return {'superreplayepisode': superreplayepisode}
+
+@register.inclusion_tag('superreplayepisode_basic_list_tag.html')
+def superreplayepisode_basic_list_display(superreplayepisode_list):
+    return {'superreplayepisode_list': superreplayepisode_list}
