cf0f9aac9997f1a1c0df7602bde17685d439cf95
toddbrentlinger <toddbrentlinger@gmail.com>
2022-08-22 18:40:09 -0700

    Move Episode stylesheet and tags to episodes app
    Moves template tag to display heading json into episodes app.
    Creates episode common stylesheet imported by episode detail template.
    Extends episode detail template for ReplayEpisode and SuperReplayEpisode.

--- episodes/templatetags/episodes_tags.py
+++ episodes/templatetags/episodes_tags.py
@@ -11,3 +11,14 @@ def episode_basic_list_display(episode_page_obj):
     return {
         'episode_page_obj': episode_page_obj
     }
+
+@register.inclusion_tag('heading_tag.html')
+def display_heading_json(key, value):
+    if key == 'quotes':
+        def handle_quotes_map(item):
+            if isinstance(item, list):
+                return '\n\n'.join(item)
+            else:
+                return item
+        value = map(handle_quotes_map, value)
+    return {'key': key, 'value': value}
--- replay/templatetags/replayepisode_tags.py
+++ replay/templatetags/replayepisode_tags.py
@@ -9,14 +9,3 @@ def replayepisode_basic_display(replayepisode):
 @register.inclusion_tag('replayepisode_basic_list_tag.html')
 def replayepisode_basic_list_display(replayepisode_page_obj):
     return {'episode_page_obj': replayepisode_page_obj}
-
-@register.inclusion_tag('heading_tag.html')
-def display_heading_json(key, value):
-    if key == 'quotes':
-        def handle_quotes_map(item):
-            if isinstance(item, list):
-                return '\n\n'.join(item)
-            else:
-                return item
-        value = map(handle_quotes_map, value)
-    return {'key': key, 'value': value}
