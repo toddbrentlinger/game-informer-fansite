6ca3c81e4b3221fbaf60c6a1bbc55b5fbba895ee
toddbrentlinger <toddbrentlinger@gmail.com>
2022-08-26 20:09:54 -0700

    Add page to display episodes NOT part of any shows
    Creates page to display all episodes that have an empty 'shows' property.
    Adds link to page that display list of shows titled 'other'.

--- shows/views.py
+++ shows/views.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Show
+from episodes.models import Episode
 
 class ShowListView(generic.ListView):
     model = Show
@@ -11,11 +12,14 @@ class ShowDetailView(generic.DetailView):
     model = Show
 
 def show_detail_slug_view(request, slug):
-    show = get_object_or_404(Show, slug=slug)
+    if slug is None or slug == 'other':
+        show = None
+        show_episodes_list = Episode.objects.filter(shows__isnull=True)
+    else:
+        show = get_object_or_404(Show, slug=slug)
+        show_episodes_list = show.episode_set.all()
 
-    show_episodes_list = show.episode_set.all()
     paginator = Paginator(show_episodes_list, 20)
-
     page_number = request.GET.get('page')
     episode_page_obj = paginator.get_page(page_number)
 
