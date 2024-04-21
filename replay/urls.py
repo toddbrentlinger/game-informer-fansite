0a0c8b48509e76c2f3230b56e1d73aac6494e6d5
toddbrentlinger <toddbrentlinger@gmail.com>
2024-04-21 13:01:33 -0700

    Update replay app urls and views
    Creates utility function that extends base create_sort_filter_context in episode app, used
    to display list of episodes, to create context that updates sort queries and add new sort
    option for Replay number. This utility function is used to create context for views that
    display list of all ReplayEpisodes, filtered by SegmentType.
    Adds view that uses custom Manager in ReplayEpisode to find specific ReplayEpisode
    given season and season episode numbers as slug.
    Adds comments and docstrings to function views.
    Removes unused generic views that were replaced with custom function views.

--- replay/urls.py
+++ replay/urls.py
@@ -3,14 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.replay_episode_list_view, name='replay'),
-    # path('<int:pk>/', views.ReplayEpisodeDetailView.as_view(), name='replay-detail'),
     path('<int:pk>/', views.replay_episode_detail_view, name='replay-detail'),
-    path('s<int:season>/<int:number>/', views.ReplayEpisodeDetailView.as_view()),
+    path('s<int:season>/<int:number>/', views.replay_episode_detail_season_episode_view, name='replay-detail-season-episode'),
 
     # Replay Segments
     path('segments/', views.segment_type_list_view, name='segments'),
-    # path('segments/', views.SegmentTypeListView.as_view(), name='segments'),
-    path('segments/<int:pk>', views.SegmentTypeDetailView.as_view(), name='segment-type-detail'),
 
     # Regex
     re_path(r'segments/(?P<slug>[-\w]+)/$', views.segment_type_detail_slug_view, name='segment-type-detail-slug'),
@@ -18,5 +15,4 @@ urlpatterns = [
 
     # AJAX
     path('get/ajax/random-replay-episode', views.get_random_replay_episode, name='get_random_replay_episode'),
-
 ]
--- replay/views.py
+++ replay/views.py
@@ -3,54 +3,83 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
 from django.views import generic
-from .models import ReplayEpisode, Segment, SegmentType
+from .models import ReplayEpisode, SegmentType
+from episodes.views import creat_sort_filter_context
 from random import choice
 
-# Create your views here.
+# Utility Functions
 
-class ReplayEpisodeListView(generic.ListView):
-    model = ReplayEpisode
-    paginate_by = 20
-
-def replay_episode_list_view(request):
-    replay_episode_list = ReplayEpisode.objects.all()
-    paginator = Paginator(replay_episode_list, 20)
-
-    page_number = request.GET.get('page')
-    replay_episode_page_obj = paginator.get_page(page_number)
-
-    context = {
-        'episode_page_obj': replay_episode_page_obj,
+def creat_sort_filter_context_replay(request, query_set):
+    # Add 'replay' sort option that sorts ReplayEpisodes by replay episode number.
+    # Also add 'show_episode__episode__' prefix to sort options that reference base Episode fields.
+    SORT_UPDATES = {
+        'replay': 'number',
+        'airdate': 'show_episode__episode__airdate',
+        'likes': 'show_episode__episode__youtube_video__likes',
+        'video-length': 'show_episode__episode__youtube_video__duration',
+        'views': 'show_episode__episode__youtube_video__views',
     }
 
+    # Create base context to display list passing in request, query_set, 
+    # SORT_UPDATES for this app, and to sort by ReplayEpisode number by 
+    # default.
+    return creat_sort_filter_context(
+        request, 
+        query_set, 
+        '',
+        SORT_UPDATES, 
+        'replay'
+    )
+
+# Replay List View
+
+def replay_episode_list_view(request):
+    """Return HttpResponse to display list of ReplayEpisodes that are filtered and sorted depending on request."""
+    # Create base context to display list passing in all ReplayEpisodes as the 
+    # QuerySet.
+    context = creat_sort_filter_context_replay(
+        request, 
+        ReplayEpisode.objects.all(), 
+    )
+    
     return render(request, 'replay/replayepisode_list.html', context=context)
 
-class ReplayEpisodeDetailView(generic.DetailView):
-    model = ReplayEpisode
+# ReplayEpisode Detail Views
 
 def replay_episode_detail_view_base(request, replayepisode):
+    """Return HttpResponse to display single ReplayEpisode detail, given ReplayEpisode."""
     context = {
         'episode': replayepisode.show_episode.episode,
         'replayepisode': replayepisode
     }
+
     return render(request, 'replay/replayepisode_detail.html', context=context)
 
 def replay_episode_detail_view(request, pk):
+    """Return HttpResponse to display single ReplayEpisode detail, given ReplayEpisode, given primary key for specific ReplayEpisode."""
     replayepisode = get_object_or_404(ReplayEpisode, pk=pk)
     return replay_episode_detail_view_base(request, replayepisode)
 
+def replay_episode_detail_season_episode_view(request, season, number):
+    """Return HttpResponse to display single ReplayEpisode detail, given season and season episode for specific ReplayEpisode."""
+    # Use custom Manager in ReplayEpisode to find ReplayEpisode with season and season episode number
+    replayepisode = ReplayEpisode.objects.with_season_and_episode(season, number)
+    return replay_episode_detail_view_base(request, replayepisode)
+
 def replay_episode_detail_slug_view(request, slug):
+    """Return HttpResponse to display single ReplayEpisode detail, given slug for specific ReplayEpisode."""
     replayepisode = get_object_or_404(ReplayEpisode, show_episode__slug=slug)
     return replay_episode_detail_view_base(request, replayepisode)
 
-class SegmentTypeListView(generic.ListView):
-    model = SegmentType
-    paginate_by = 20
+# Replay Segment Views
 
 def segment_type_list_view(request):
+    """Return a HttpResponse to display list of Replay segments."""
+    # Create QuerySet of all segments
     segmenttype_list = SegmentType.objects.all()
-    paginator = Paginator(segmenttype_list, 20)
 
+    # Create Page object with QuerySet of all segments
+    paginator = Paginator(segmenttype_list, 25)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -60,32 +89,25 @@ def segment_type_list_view(request):
 
     return render(request, 'replay/segmenttype_list.html', context=context)
 
-class SegmentTypeDetailView(generic.DetailView):
-    model = SegmentType
-
 def segment_type_detail_slug_view(request, slug):
+    """Return a HttpResponse to display list of ReplayEpisodes that are part of a single SegmentType, given slug for specific SegmentType."""
     segmenttype = get_object_or_404(SegmentType, slug=slug)
 
-    replayepisode_list = ReplayEpisode.objects.filter(other_segments__type=segmenttype.id).distinct()
-    paginator = Paginator(replayepisode_list, 20)
+    # Create base context to display list passing in ReplayEpisodes with 
+    # matching SegmentType as the QuerySet.
+    context = creat_sort_filter_context_replay(
+        request, 
+        ReplayEpisode.objects.filter(other_segments__type=segmenttype.id).distinct(), 
+    )
 
-    page_number = request.GET.get('page')
-    replayepisode_page_obj = paginator.get_page(page_number)
-
-    context = {
-        'segmenttype': segmenttype,
-        'episode_page_obj': replayepisode_page_obj,
-    }
-
-    # context = {
-    #     'segmenttype': segmenttype,
-    #     'replayepisode_list': replayepisode_list
-    # }
-    
     return render(request, 'replay/segmenttype_detail.html', context=context)
 
+# Random ReplayEpisode from AJAX request
+
 def get_random_replay_episode_inst():
+    """Return a random instance of ReplayEpisode OR None if no ReplayEpisodes in database."""
     pks = ReplayEpisode.objects.values_list('pk', flat=True)
+    
     if pks:
         random_pk = choice(pks)
         return ReplayEpisode.objects.get(pk=random_pk)
@@ -93,17 +115,18 @@ def get_random_replay_episode_inst():
         return None
 
 def get_random_replay_episode(request):
-    if request.method == 'GET':
-        replayepisode = get_random_replay_episode_inst()
-        response = HttpResponse(
-            render_to_string(
-                'replayepisode_basic_tag.html',
-                context={
-                    'episode': replayepisode.show_episode.episode,
-                    'replayepisode': replayepisode,
-                }
-            )
-        )
-        return response
-    else:
+    """Return a HttpResponse from an AJAX request for random Replay episode."""
+    if request.method != 'GET':
         return HttpResponse('Request method is NOT a GET')
+    
+    replayepisode = get_random_replay_episode_inst()
+
+    return HttpResponse(
+        render_to_string(
+            'replayepisode_basic_tag.html',
+            context={
+                'episode': replayepisode.show_episode.episode,
+                'replayepisode': replayepisode,
+            }
+        )
+    )
