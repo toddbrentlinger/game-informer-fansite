b07a0eb7785e1656d511ddc570911dd06392acb2
toddbrentlinger <toddbrentlinger@gmail.com>
2024-04-24 00:55:06 -0700

    Fix template and tag issues in replay app
    Uses create_episode_basic_list_display_context in episode app tags to create context to display list of ReplayEpisode. Also adds extra sort option to sort by Replay number.
    Passes request.GET in template as get_query argument for create_page_selection tag in fansite app when displaying ReplayEpisode list filtered by SegmentType.

--- replay/templatetags/replayepisode_tags.py
+++ replay/templatetags/replayepisode_tags.py
@@ -1,14 +1,22 @@
 from django import template
+from episodes.templatetags.episodes_tags import create_episode_basic_list_display_context
 
 register = template.Library()
 
 @register.inclusion_tag('replayepisode_basic_tag.html')
 def replayepisode_basic_display(replayepisode):
+    """Tag to display single Replay episode data."""
     return {
         'episode': replayepisode.show_episode.episode,
         'replayepisode': replayepisode
     }
 
 @register.inclusion_tag('replayepisode_basic_list_tag.html')
-def replayepisode_basic_list_display(replayepisode_page_obj):
-    return {'episode_page_obj': replayepisode_page_obj}
+def replayepisode_basic_list_display(replayepisode_page_obj, sort, filter, get_query):
+    """Tag to display list of Replay episodes with sort and filter functionality."""
+    context = create_episode_basic_list_display_context(replayepisode_page_obj, sort, filter, get_query)
+    
+    # Add sort option that sorts by Replay episode number
+    context['sort_options_dict']['replay'] = 'Episode Number'
+    
+    return context
