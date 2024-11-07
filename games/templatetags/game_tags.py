a858e5244d01c80f15d6df273049eaa02f1dfc8f
toddbrentlinger <toddbrentlinger@gmail.com>
2024-11-06 17:08:30 -0800

    Add get_query parameter to template tags that display list of items
    Adds get_query parameter to template tags that display list of items to be used
    in create_page_selection template tag for pagination.
    Passes the request.GET value to the template tags that display list of items in
    HTML templates.

--- games/templatetags/game_tags.py
+++ games/templatetags/game_tags.py
@@ -3,5 +3,8 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('game_basic_list_tag.html')
-def game_basic_list_display(page_obj):
-    return {'page_obj': page_obj}
+def game_basic_list_display(page_obj, get_query):
+    return {
+        'page_obj': page_obj,
+        'get_query': get_query
+    }
--- people/templatetags/person_tags.py
+++ people/templatetags/person_tags.py
@@ -7,5 +7,8 @@ def person_basic_display(person):
     return {'person': person}
 
 @register.inclusion_tag('person_basic_list_tag.html')
-def person_basic_list_display(page_obj):
-    return {'page_obj': page_obj}
+def person_basic_list_display(page_obj, get_query):
+    return {
+        'page_obj': page_obj,
+        'get_query': get_query
+    }
